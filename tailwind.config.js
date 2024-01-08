84d108116227c81ded198dac3caeeb4a07d6cf9c
GeniuX <geniuh7@gmail.com>
2024-01-07 18:19:18 -0800

    creating components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple-dark': '#8874CC', // Reemplaza con tu código de color personalizado
+        'purple-light': '#4100CC', // Reemplaza con tu código de color personalizado
+        'yellow-dark': '#D1C97D', // Reemplaza con tu código de color personalizado
+        'yellow-light': '#CCC600', // Reemplaza con tu código de color personalizado
+      },
+    },
   },
   plugins: [
       require('@tailwindcss/typography'),
