5abbebd6503ede9aa552d124769bea24d6fe4ff3
Geniuh <geniuh1@gmail.com>
2024-06-09 13:13:54 -0500

    mejora

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    base:'https://GeniuX-arch.github.io/portafolioweb',
     integrations: [tailwind()],
 });
