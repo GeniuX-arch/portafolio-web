30e9e8bc7b59246a27e2d0262ab0a102d19a9d93
Geniuh <geniuh1@gmail.com>
2024-06-09 17:34:59 -0500

    mejora imagenes

--- public/tecnologias.js
+++
@@ -1,7 +0,0 @@
-import email from "./email.svg"
-import favicon from "./favicon.svg"
-import github from "./github.svg"
-import gmail from "./gmail.svg"
-import linkeding from "./linkedin.svg"
-
-export {email,favicon,github,gmail,linkeding}
