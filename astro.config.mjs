899c06b5e540aca4d45edf3690ee1d1bc0a708b4
Geniuh <geniuh1@gmail.com>
2024-06-10 14:03:11 -0500

    mejora para publicar en github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://GeniuX-arch.github.io/portafolioweb',
+    site: 'https://GeniuX-arch.github.io/',
     base: '/portafolioweb',
-    buildOptions: {
-        site: 'https://geniux-arch.github.io/portafolioweb',
-        outDir: 'dist',
-      },
     integrations: [tailwind()],
 });
